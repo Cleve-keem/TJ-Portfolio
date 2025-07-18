b474ca550a4b8928a8f8f0e004440ce8522a8991
Cleve-keem <belloharyourmidey@gmail.com>
2025-07-18 18:46:21 -0500

    added a small  height

--- src/features/Main/components/Main.jsx
+++ src/features/Main/components/Main.jsx
@@ -3,11 +3,11 @@ export default function Main() {
     <main className="p-7 text-white" id="home">
       {/* Background layer */}
       <div
-        className="fixed inset-0 bg-[url('/bg-image.jpg')] bg-cover bg-top bg-no-repeat -z-10 transition-all duration-500 ease-in-out"
+        className="fixed inset-0 bg-[url('/bg-image.jpg')] bg-cover bg-center bg-no-repeat -z-10 transition-all duration-500 ease-in-out"
         aria-hidden="true"
       />
 
-      <section className="flex flex-col h-dvh">
+      <section className="flex flex-col h-svh">
         <div className="flex items-center gap-2">
           <span className="block size-2 bg-green-500 rounded-full"></span>
           <p className="font-semibold">Open to work</p>
