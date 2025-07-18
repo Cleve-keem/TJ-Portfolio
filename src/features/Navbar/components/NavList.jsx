f51efcb8a1555143c067d97e501595d20ee49793
Cleve-keem <belloharyourmidey@gmail.com>
2025-07-18 12:14:16 -0500

    made the navbar responsive

--- src/features/Navbar/components/NavList.jsx
+++ src/features/Navbar/components/NavList.jsx
@@ -5,8 +5,8 @@ export default function NavList({ icon, label }) {
         href="#home"
         className="flex items-center justify-center gap-1 py-4 rounded-4xl text-white text-[14px]"
       >
-        <div className="text-inherit">{icon}</div>
-        <span>{label}</span>
+        <div className="text-2xl md:text-inherit">{icon}</div>
+        <span className="hidden md:block">{label}</span>
       </a>
     </li>
   );
